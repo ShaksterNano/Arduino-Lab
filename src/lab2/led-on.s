5fea6636e8d8925471fc5c5412e943fae21e031a
ShaksterNano <shukai.zhang@hotmail.com>
2022-03-10 13:27:51 +0000

    Update led-on.s

--- src/lab2/led-on.s
+++ src/lab2/led-on.s
@@ -1,16 +1,16 @@
-; led_on.s : switches on the arduino LED
+; Switches on the Arduino LED
 
-; specify equivalent symbols
+; Specify equivalent symbols
 .equ SREG, 0x3f             ; Status register, address 0 x3f. See data sheet , p.11
 .equ DDRB, 0x04
 .equ PORTB, 0x05
 
-; specify the start address
+; Specify the start address
 .org 0
-; reset system status
-main:   ldi r16, 0          ; set register r16 to zero
-        out SREG, r16       ; copy contents of r16 to SREG, i.e., clear SREG.
-        ldi r16, 0b00111111
+; Reset system status
+main:   ldi r16, 0          ; Set register r16 to zero
+        out SREG, r16       ; Copy contents of r16 to SREG, i.e., clear SREG.
+        ldi r16, 0b00111111 ; Turns on pins D8 - D13
         out DDRB, r16
         out PORTB, r16
-mainloop: rjmp mainloop    ; jump back to mainloop address
+mainloop: rjmp mainloop     ; Jump back to mainloop address
