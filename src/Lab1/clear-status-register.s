dcbaf158eed84ef8a7c399819584c7bc724074c4
ShakterNano <54268387+ShakterNano@users.noreply.github.com>
2022-03-10 13:40:49 +0000

    Delete src/Lab1 directory

--- src/Lab1/clear-status-register.s
+++
@@ -1,5 +0,0 @@
-.equ SREG, 0x3f ; define SREG label
-.org 0
-main:   ldi r16, 0 ; set register r16 to zero
-        out SREG, r16 ; copy contents of r16 to SREG
-mainloop: rjmp mainloop ; jump to mainloop address
